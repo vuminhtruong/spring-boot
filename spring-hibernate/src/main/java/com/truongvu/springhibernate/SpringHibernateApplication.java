56c1c7e7b37d39cfd32f253b30dab09041cb215e
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-11-26 15:54:20 +0700

    update main_app

--- spring-hibernate/src/main/java/com/truongvu/springhibernate/SpringHibernateApplication.java
+++ spring-hibernate/src/main/java/com/truongvu/springhibernate/SpringHibernateApplication.java
@@ -1,5 +1,7 @@
 package com.truongvu.springhibernate;
 
+import com.truongvu.springhibernate.dao.StudentDAO;
+import com.truongvu.springhibernate.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,20 @@ public class SpringHibernateApplication {
     }
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello World");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Truong","Vu","test123@gmail.com");
+
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent);
+
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 	}
 
 }
