299daea3dd78ea33c9d6b92ae52a1f938324bb61
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-11-26 11:09:28 +0700

    jpa annotation

--- spring-hibernate/src/main/java/com/truongvu/springhibernate/SpringHibernateApplication.java
+++ spring-hibernate/src/main/java/com/truongvu/springhibernate/SpringHibernateApplication.java
@@ -1,13 +1,22 @@
 package com.truongvu.springhibernate;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringHibernateApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringHibernateApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHibernateApplication.class, args);
+    }
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World");
+		};
 	}
 
 }
---
+++ spring-hibernate/src/main/java/com/truongvu/springhibernate/entity/Student.java
@@ -0,0 +1,72 @@
+package com.truongvu.springhibernate.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String latName;
+
+    @Column(name="email")
+    private String email;
+
+    public Student() {
+
+    }
+
+    public Student(String firstName, String latName, String email) {
+        this.firstName = firstName;
+        this.latName = latName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLatName() {
+        return latName;
+    }
+
+    public void setLatName(String latName) {
+        this.latName = latName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", latName='" + latName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
