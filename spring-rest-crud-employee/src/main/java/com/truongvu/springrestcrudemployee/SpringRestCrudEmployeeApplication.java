5bae61e02a1920666715202d40a2bb345b32dd4b
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-11-28 21:46:21 +0700

    create project spring-rest-crud-2.0

---
+++ spring-rest-crud-employee/src/main/java/com/truongvu/springrestcrudemployee/SpringRestCrudEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.truongvu.springrestcrudemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestCrudEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestCrudEmployeeApplication.class, args);
+	}
+
+}
---
+++ spring-rest-crud-employee/src/test/java/com/truongvu/springrestcrudemployee/SpringRestCrudEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truongvu.springrestcrudemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestCrudEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
