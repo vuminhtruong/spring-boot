57ee0e6abd38a5f2d77be1e849057b27bc13e9b3
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-11-28 18:23:00 +0700

    path variable annotation

--- spring-rest-crud/src/main/java/com/truongvu/springrestcrud/rest/StudentRestController.java
+++ spring-rest-crud/src/main/java/com/truongvu/springrestcrud/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.truongvu.springrestcrud.rest;
 
 import com.truongvu.springrestcrud.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +13,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    private final List<Student> studentList = new ArrayList<>();
 
-    @GetMapping("/student")
-    public List<Student> getStudent() {
-        List<Student> studentList = new ArrayList<>();
-
+    @PostConstruct
+    public void loadData() {
         studentList.add(new Student("Truong","Vu"));
         studentList.add(new Student("Nam","Tran"));
         studentList.add(new Student("Trung","Nguyen"));
+    }
 
+    @GetMapping("/student")
+    public List<Student> getStudent() {
         return studentList;
+    }
+
+    @GetMapping("/student/{studentID}")
+    public Student getStudent(@PathVariable int studentID) {
+        return studentList.get(studentID);
     }
 }
