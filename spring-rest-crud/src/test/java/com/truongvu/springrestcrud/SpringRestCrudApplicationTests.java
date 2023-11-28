b978f85dfaa99862505a0ecc462301e795284cab
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-11-28 18:06:50 +0700

    create spring-rest-crud project

---
+++ spring-rest-crud/src/test/java/com/truongvu/springrestcrud/SpringRestCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truongvu.springrestcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
