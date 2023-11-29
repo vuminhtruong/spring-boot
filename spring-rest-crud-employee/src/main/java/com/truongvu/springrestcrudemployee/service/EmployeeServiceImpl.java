8d7e60d038670969fbca28873fd225a3da13e2ab
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-11-29 10:01:49 +0700

    add Service method

--- spring-rest-crud-employee/src/main/java/com/truongvu/springrestcrudemployee/service/EmployeeServiceImpl.java
+++ spring-rest-crud-employee/src/main/java/com/truongvu/springrestcrudemployee/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.truongvu.springrestcrudemployee.dao.EmployeeDAO;
 import com.truongvu.springrestcrudemployee.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,5 +19,22 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return employeeDAO.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee employee) {
+        return employeeDAO.save(employee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int id) {
+        employeeDAO.deleteById(id);
     }
 }
