42e9d9039eff4f3639ea591d165f5a9efe190bf1
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-12-04 17:03:17 +0700

    create spring-mvc-thymeleaf project

---
+++ spring-mvc-thymeleaf/src/test/java/com/truongvu/springmvcthymeleaf/SpringMvcThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truongvu.springmvcthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
