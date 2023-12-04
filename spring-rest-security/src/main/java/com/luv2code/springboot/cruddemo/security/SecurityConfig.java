42b1672b3525e4302f29eb2a0fc99ea974d5f94a
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-12-04 15:37:35 +0700

    secutiry configuration to use JDBC

--- spring-rest-security/src/main/java/com/luv2code/springboot/cruddemo/security/SecurityConfig.java
+++ spring-rest-security/src/main/java/com/luv2code/springboot/cruddemo/security/SecurityConfig.java
@@ -9,31 +9,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager();
     }
 
     @Bean
@@ -49,6 +35,5 @@ public class SecurityConfig {
 
         httpSecurity.httpBasic(Customizer.withDefaults());
 
-        return httpSecurity.build();
-    }
+        return httpSecurity.build();    }
 }
