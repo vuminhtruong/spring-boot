c8305bfd20a966828bcc9c1189865cd603a91b11
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-12-04 10:04:00 +0700

    create project spring-security

--- spring-rest-crud-employee/src/main/java/com/truongvu/springrestcrudemployee/dao/EmployeeRepository.java
+++ spring-rest-crud-employee/src/main/java/com/truongvu/springrestcrudemployee/dao/EmployeeRepository.java
@@ -3,5 +3,6 @@ package com.truongvu.springrestcrudemployee.dao;
 import com.truongvu.springrestcrudemployee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
