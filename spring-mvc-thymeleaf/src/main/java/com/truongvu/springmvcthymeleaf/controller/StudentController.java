d3bd64313c2686aa6edd8adf83cf683a42ba9fc3
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-12-04 21:28:43 +0700

    use DropDown lists

--- spring-mvc-thymeleaf/src/main/java/com/truongvu/springmvcthymeleaf/controller/StudentController.java
+++ spring-mvc-thymeleaf/src/main/java/com/truongvu/springmvcthymeleaf/controller/StudentController.java
@@ -1,18 +1,26 @@
 package com.truongvu.springmvcthymeleaf.controller;
 
 import com.truongvu.springmvcthymeleaf.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model model) {
         Student student = new Student();
         model.addAttribute("student", student);
+        model.addAttribute("countries", countries);
         return "student-form";
     }
 
--- spring-mvc-thymeleaf/src/main/java/com/truongvu/springmvcthymeleaf/model/Student.java
+++ spring-mvc-thymeleaf/src/main/java/com/truongvu/springmvcthymeleaf/model/Student.java
@@ -3,6 +3,7 @@ package com.truongvu.springmvcthymeleaf.model;
 public class Student {
     private String firstName;
     private String lastName;
+    private String country;
 
     public Student() {
 
@@ -22,5 +23,13 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 }
