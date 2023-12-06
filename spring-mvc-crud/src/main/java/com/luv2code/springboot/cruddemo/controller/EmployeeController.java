4eda9f4a73736b4fbcfbdb30106ef45ad586e8d6
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-12-06 21:23:20 +0700

    create spring-mvc controller

---
+++ spring-mvc-crud/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package com.luv2code.springboot.cruddemo.controller;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model) {
+        List<Employee> employees = employeeService.findAll();
+        model.addAttribute("employees",employees);
+        return "list-employees";
+    }
+}
