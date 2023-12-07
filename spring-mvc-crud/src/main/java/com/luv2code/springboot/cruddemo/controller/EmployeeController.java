b250d9dd600a80dc63197ff5385ee1b04ecc7713
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-12-07 10:07:32 +0700

    add new employee

--- spring-mvc-crud/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
+++ spring-mvc-crud/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,6 +24,19 @@ public class EmployeeController {
     public String listEmployees(Model model) {
         List<Employee> employees = employeeService.findAll();
         model.addAttribute("employees",employees);
-        return "list-employees";
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee",employee);
+        return "employees/employee-form";
+    }
+
+    @PostMapping("save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.save(employee);
+        return "redirect:/employees/list";
     }
 }
--- spring-mvc-crud/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ spring-mvc-crud/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,9 +2,11 @@ package com.luv2code.springboot.cruddemo.dao;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code LOL!
 
+    public List<Employee> findAllByOrderByLastNameAsc();
 }
