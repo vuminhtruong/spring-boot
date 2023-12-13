7cf3e3f5409a2030068823ba1d989a386f1b7429
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-12-13 10:45:21 +0700

    custom table JDBC authentication

--- spring-mvc-security/src/main/java/com/example/truongvu/springmvcsecurity/security/DemoSecurityConfig.java
+++ spring-mvc-security/src/main/java/com/example/truongvu/springmvcsecurity/security/DemoSecurityConfig.java
@@ -39,6 +39,13 @@ public class DemoSecurityConfig {
 
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?"
+        );
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?"
+        );
         return new JdbcUserDetailsManager(dataSource);
     }
 
