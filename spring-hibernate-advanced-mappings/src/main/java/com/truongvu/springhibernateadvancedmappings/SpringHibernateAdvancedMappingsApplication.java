0458dde3313da2eecf5e79c94478764edc1a8b5b
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-12-19 11:01:05 +0700

    create project

---
+++ spring-hibernate-advanced-mappings/src/main/java/com/truongvu/springhibernateadvancedmappings/SpringHibernateAdvancedMappingsApplication.java
@@ -0,0 +1,13 @@
+package com.truongvu.springhibernateadvancedmappings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateAdvancedMappingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateAdvancedMappingsApplication.class, args);
+	}
+
+}
---
+++ spring-hibernate-advanced-mappings/src/test/java/com/truongvu/springhibernateadvancedmappings/SpringHibernateAdvancedMappingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truongvu.springhibernateadvancedmappings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateAdvancedMappingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
