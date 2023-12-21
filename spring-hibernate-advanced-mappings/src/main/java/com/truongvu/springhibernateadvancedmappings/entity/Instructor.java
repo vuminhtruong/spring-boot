90214ccd719eae99520ace7c04c30923ca100f1d
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-12-21 21:07:05 +0700

    update instructor class

--- spring-hibernate-advanced-mappings/src/main/java/com/truongvu/springhibernateadvancedmappings/entity/Instructor.java
+++ spring-hibernate-advanced-mappings/src/main/java/com/truongvu/springhibernateadvancedmappings/entity/Instructor.java
@@ -2,6 +2,9 @@ package com.truongvu.springhibernateadvancedmappings.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "instructor")
 public class Instructor {
@@ -24,6 +27,9 @@ public class Instructor {
     @JoinColumn(name = "instructor_detail_id")
     private InstructorDetail instructorDetail;
 
+    @OneToMany(mappedBy = "instructor",cascade = {CascadeType.PERSIST,CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH})
+    private List<Course> courses;
+
     public Instructor() {
 
     }
@@ -32,6 +38,14 @@ public class Instructor {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
     }
 
     public int getId() {
@@ -83,5 +97,14 @@ public class Instructor {
                 ", email='" + email + '\'' +
                 ", instructorDetail=" + instructorDetail +
                 '}';
+    }
+
+    public void addCourse(Course course) {
+        if(this.courses == null) {
+            courses = new ArrayList<>();
+        }
+
+        courses.add(course);
+        course.setInstructor(this);
     }
 }
