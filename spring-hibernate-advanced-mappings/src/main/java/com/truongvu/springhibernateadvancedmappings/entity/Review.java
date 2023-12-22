cbeb241e66e5cced4e3d4b1f1ee25eaefeebabea
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-12-22 11:25:33 +0700

    create Review entity

--- spring-hibernate-advanced-mappings/src/main/java/com/truongvu/springhibernateadvancedmappings/entity/Review.java
+++ spring-hibernate-advanced-mappings/src/main/java/com/truongvu/springhibernateadvancedmappings/entity/Review.java
@@ -1,2 +1,48 @@
-package com.truongvu.springhibernateadvancedmappings.entity;public class Review {
+package com.truongvu.springhibernateadvancedmappings.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "review")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "Comment")
+    private String comment;
+
+    public Review() {
+
+    }
+
+    public Review(String comment) {
+        this.comment = comment;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "id=" + id +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
 }
