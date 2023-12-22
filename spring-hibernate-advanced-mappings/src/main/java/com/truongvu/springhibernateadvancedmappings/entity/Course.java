9d8d5d98616ce5711526405630dbb4616e412b26
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-12-22 20:44:46 +0700

    map course to student

--- spring-hibernate-advanced-mappings/src/main/java/com/truongvu/springhibernateadvancedmappings/entity/Course.java
+++ spring-hibernate-advanced-mappings/src/main/java/com/truongvu/springhibernateadvancedmappings/entity/Course.java
@@ -25,12 +25,28 @@ public class Course {
     @JoinColumn(name = "course_id")
     private List<Review> reviews;
 
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinTable(
+            name = "course_student",
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    private List<Student> students;
+
     public Course() {
 
     }
 
     public Course(String title) {
         this.title = title;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
     }
 
     public List<Review> getReviews() {
@@ -78,5 +94,13 @@ public class Course {
             reviews = new ArrayList<>();
         }
         reviews.add(review);
+    }
+
+    public void addStudent(Student student) {
+        if(students == null) {
+            students = new ArrayList<>();
+        }
+
+        students.add(student);
     }
 }
